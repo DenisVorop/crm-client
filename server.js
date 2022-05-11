c04037ddd3188e546df9387f1a701b3ba4d8065e
DenisVorop <saskeazaske@icloud.com>
2022-05-11 19:13:42 +0300

    deploy to heroku

--- server.js
+++ server.js
@@ -4,14 +4,28 @@ const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
     static: './build',
-    jsonServerAuth,
 });
 
 const PORT = process.env.PORT || 4444;
 
 server.use(middlewares);
+server.use(jsonServerAuth);
 server.use(router);
 
 server.listen(PORT, () => {
     console.log('Server is running');
 });
+
+// const jsonServer = require('json-server')
+// const auth = require('json-server-auth')
+
+// const app = jsonServer.create()
+// const router = jsonServer.router('db.json')
+
+// // /!\ Bind the router db to the app
+// app.db = router.db
+
+// // You must apply the auth middleware before the router
+// app.use(auth)
+// app.use(router)
+// app.listen(3000)
